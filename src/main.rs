c885bc4f8406a88c81ecf77cc3342753af40597b
Ryan Levick <ryan.levick@gmail.com>
2020-05-18 12:04:50 +0200

    Switch to hyper

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,13 @@
-use async_std::task;
 use cargo_toml::{Manifest, Value};
-use std::path::{Path, PathBuf};
+use futures::future::{BoxFuture, FutureExt};
 use structopt::StructOpt;
 use thiserror::Error;
 
+use std::ffi::OsString;
+use std::io::Read;
+use std::path::{Path, PathBuf};
+
 fn main() {
-    // let reader = std::fs::File::open("./win2d.uwp.1.25.0.nupkg").unwrap();
-    // let mut zip = zip::ZipArchive::new(reader).unwrap();
-    // for i in 0..zip.len() {
-    //     let file = zip.by_index(i).unwrap();
-    //     println!("Filename: {}", file.name());
-    //     // let first_byte = file.bytes().next().unwrap()?;
-    //     // println!("{}", first_byte);
-    // }
     let opt = Opt::from_args();
     match opt.subcommand {
         Subcommand::Install(i) => i.perform().unwrap(),
@@ -53,34 +48,16 @@ impl Install {
         let manifest = Manifest::from_slice(&bytes).map_err(|_| Error::MalformedManifest)?;
         let deps = get_deps(manifest)?;
         let downloaded_deps = download_dependencies(deps)?;
-        for (dep, zipped_bytes) in downloaded_deps {
-            let reader = std::io::Cursor::new(zipped_bytes);
-            let mut zip = zip::ZipArchive::new(reader).map_err(|e| Error::Other(Box::new(e)))?;
-            let mut winmds = Vec::new();
-            for i in 0..zip.len() {
-                let mut file = zip.by_index(i).unwrap();
-                let path = file.sanitized_name();
-                match path.extension() {
-                    Some(e) if e == "winmd" => {
-                        let name = path.file_name().unwrap().to_owned();
-                        let mut contents = Vec::with_capacity(file.size() as usize);
-                        use std::io::Read;
-                        println!("{:?}", name);
-                        if let Err(e) = file.read_to_end(&mut contents) {
-                            println!("Could not read file: {:?} {:?}", e, file.compression());
-                            continue;
-                        }
-                        winmds.push((name, contents));
-                    }
-                    _ => {}
-                }
-                // todo: DLLs
-            }
-            let dep_directory = PathBuf::new().join("target").join("nuget").join(dep.name);
+        for dep in downloaded_deps {
+            let winmds = dep.winmds()?;
+            let dep_directory = PathBuf::new()
+                .join("target")
+                .join("nuget")
+                .join(dep.dependency.name);
             // create the dependency directory
             std::fs::create_dir_all(&dep_directory).unwrap();
-            for (name, contents) in winmds {
-                std::fs::write(&dep_directory.join(name), contents).unwrap()
+            for winmd in winmds {
+                winmd.write(&dep_directory).unwrap();
             }
         }
 
@@ -126,39 +103,104 @@ impl Dependency {
     }
 
     async fn download(&self) -> Result<Vec<u8>, Error> {
-        let mut res = surf::get(self.url())
-            .await
-            .map_err(|e| Error::DownloadError(e))?;
-        match res.status().into() {
-            200u16 => {}
-            302 => {
-                let headers = res.headers();
-                let redirect_url = headers.get("Location").unwrap();
-                res = surf::get(redirect_url).await.unwrap();
-                assert!(res.status() == 200);
+        fn try_download(
+            url: String,
+            recursion_amount: u8,
+        ) -> BoxFuture<'static, Result<Vec<u8>, Error>> {
+            async move {
+                if recursion_amount == 0 {
+                    return Err(Error::DownloadError(
+                        anyhow::anyhow!("Too many redirects").into(),
+                    ));
+                }
+                let res = reqwest::get(&url)
+                    .await
+                    .map_err(|e| Error::DownloadError(e.into()))?;
+                match res.status().into() {
+                    200u16 => {
+                        let bytes = res
+                            .bytes()
+                            .await
+                            .map_err(|e| Error::DownloadError(e.into()))?;
+                        Ok(bytes.into_iter().collect())
+                    }
+                    302 => {
+                        let headers = res.headers();
+                        let redirect_url = headers.get("Location").unwrap();
+
+                        let url = redirect_url.to_str().unwrap();
+
+                        try_download(url.to_owned(), recursion_amount - 1).await
+                    }
+                    _ => {
+                        return Err(Error::DownloadError(
+                            anyhow::anyhow!("Non-successful response: {}", res.status()).into(),
+                        ))
+                    }
+                }
             }
-            _ => {
-                return Err(Error::DownloadError(
-                    anyhow::anyhow!("Not 200 response: {}", res.status()).into(),
-                ))
-            }
+            .boxed()
         }
 
-        let bytes = res
-            .body_bytes()
-            .await
-            .map_err(|e| Error::DownloadError(e.into()))?;
-        Ok(bytes)
+        try_download(self.url(), 5).await
     }
 }
 
-fn download_dependencies(deps: Vec<Dependency>) -> Result<Vec<(Dependency, Vec<u8>)>, Error> {
-    task::block_on(async {
+struct DownloadedDependency {
+    dependency: Dependency,
+    bytes: Vec<u8>,
+}
+
+impl DownloadedDependency {
+    fn winmds(&self) -> Result<Vec<Winmd>, Error> {
+        let reader = std::io::Cursor::new(&self.bytes);
+        let mut zip = zip::ZipArchive::new(reader).map_err(|e| Error::Other(Box::new(e)))?;
+        let mut winmds = Vec::new();
+        for i in 0..zip.len() {
+            let mut file = zip.by_index(i).unwrap();
+            let path = file.sanitized_name();
+            match path.extension() {
+                Some(e)
+                    if e == "winmd"
+                        && path.parent().and_then(Path::to_str) == Some("lib\\uap10.0") =>
+                {
+                    let name = path.file_name().unwrap().to_owned();
+                    let mut contents = Vec::with_capacity(file.size() as usize);
+
+                    if let Err(e) = file.read_to_end(&mut contents) {
+                        eprintln!("Could not read winmd file: {:?}", e);
+                        continue;
+                    }
+                    winmds.push(Winmd { name, contents });
+                }
+                _ => {}
+            }
+        }
+        Ok(winmds)
+    }
+}
+
+fn download_dependencies(deps: Vec<Dependency>) -> Result<Vec<DownloadedDependency>, Error> {
+    tokio::runtime::Runtime::new().unwrap().block_on(async {
         let results = deps.into_iter().map(|dep| async move {
             let bytes = dep.download().await?;
-            Ok((dep, bytes))
+            Ok(DownloadedDependency {
+                dependency: dep,
+                bytes,
+            })
         });
 
         futures::future::try_join_all(results).await
     })
+}
+
+struct Winmd {
+    name: OsString,
+    contents: Vec<u8>,
+}
+
+impl Winmd {
+    fn write(&self, dir: &Path) -> std::io::Result<()> {
+        std::fs::write(dir.join(&self.name), &self.contents)
+    }
 }
